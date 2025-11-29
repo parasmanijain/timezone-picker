f2456c9ddc3b3e2b65b9934a55a8865eb412ae31
Parasmani Jain <parasmani.jain2208@gmail.com>
2025-11-29 17:38:00 +0530

    typescript packages added

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
-import { enableProdMode } from "@angular/core";
+import { provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication } from "@angular/platform-browser";
-import { environment } from "./environments/environment";
 import { AppComponent } from "./app/app.component";
 
-if (environment.production) {
-  enableProdMode();
-}
-
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideZoneChangeDetection({ eventCoalescing: true })],
+}).catch((err) => console.error(err));
