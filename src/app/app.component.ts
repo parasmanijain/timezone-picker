2c294671a7c113828d786b12c0624447f2d4e37d
Parasmani Jain <parasmani.jain2208@gmail.com>
2025-11-29 15:55:01 +0530

    packages updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { FormsModule } from "@angular/forms";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
   imports: [FormsModule],
+  standalone: true,
 })
 export class AppComponent implements OnInit {
   public dateTimeFormat = "";
